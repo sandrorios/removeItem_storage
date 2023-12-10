65e3d9b07807a28fa10951e2667844830727925a
sandrorios <sandroderios@hotmail.com>
2023-12-10 14:54:49 -0500

    created list item

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const listItem = document.getElementById('list-item');
 const clearBtn = document.getElementById('clear');
 const filter = document.getElementById('filter');
 
+function addItem(e) {
+    e.preventDefault();
+    let newItem = inputField.value;
+    if(newItem === ''){
+        alert("Please add an item");
+        return;
+    }
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(newItem));
+    listItem.appendChild(li);
+}
+
+
+formItem.addEventListener('submit', addItem);
