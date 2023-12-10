07f06acc1108a8dc7bdb7dbd1c7d06aebe2afb6e
sandrorios <sandroderios@hotmail.com>
2023-12-10 14:51:30 -0500

    added variable to link to the html

---
+++ script.js
@@ -0,0 +1,6 @@
+const formItem = document.getElementById('form-item');
+const inputField = document.getElementById('input-field');
+const listItem = document.getElementById('list-item');
+const clearBtn = document.getElementById('clear');
+const filter = document.getElementById('filter');
+
